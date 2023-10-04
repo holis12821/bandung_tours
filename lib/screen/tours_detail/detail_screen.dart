87ff5dde268fd965d051cbcfc6fda163aca64c0a
Nurholis <nur.kholis@ottencoffee.co.id>
2023-10-04 21:38:20 +0700

    add page detail

---
+++ lib/screen/tours_detail/detail_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child:  Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                 const Column(
+                    children: [
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 8.0),
+                      Text('Open Everyday'),
+                    ],
+                  ),
+                 const Column(
+                    children: [
+                      Icon(Icons.access_time),
+                      SizedBox(height: 8.0),
+                      Text('09:00 - 20:00'),
+                    ],
+                  ),
+                  const Column(
+                    children: [
+                      Icon(Icons.monetization_on),
+                      SizedBox(height: 8.0),
+                      Text('Rp 25.000'),
+                    ],
+                  ),
+                  ElevatedButton(onPressed: () {
+                      
+                  }, child: const Text('Tombol'))
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 16.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
